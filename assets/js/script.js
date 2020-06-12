42528447a827e1dfe1324f362cfbaf5673191ad1
megmog88 <meghanspence88@hotmail.co.uk>
2020-06-12 11:36:51 +0000

    added model and trigger event to show without clicking button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,7 @@
   
   //-------------Age Verification----//
+$(document).ready(function(){
 
-  let age = window.prompt("Please enter your age?: ")
-    if (age >= 18 && <= 120) {
-        window.alert("Welcome to Meg Mixology");
-    } else if (age > 120 || age < 18){
-        window.alert("Sorry, you are not authorised to enter this site")
-    } else {
-        window.alert("Unauthorised access!");
-    }
+$('.myModal').modal('show'); 
+
+});
